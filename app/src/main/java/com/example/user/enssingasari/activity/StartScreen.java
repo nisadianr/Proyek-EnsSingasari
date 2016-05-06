811a7aa87264bfed3eccc404348b5955989a46e9
nisadianr <nniso.ong@gmail.com>
2016-05-07 00:16:43 +0700

    add menu interface with image slider

--- app/src/main/java/com/example/user/enssingasari/activity/StartScreen.java
+++ app/src/main/java/com/example/user/enssingasari/activity/StartScreen.java
@@ -2,48 +2,38 @@ package com.example.user.enssingasari.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.user.enssingasari.R;
 
 //this class is for the start menu
 
 public class StartScreen extends Activity implements View.OnClickListener{
-//    private FragmentManager fragmentManager ;
-//    private FragmentTransaction transaction;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
-//        fragmentManager = getActivity().getFragmentManager();
 
         Button start = (Button) findViewById(R.id.start_button);
         start.setOnClickListener(this);
+
+        Resources res = getResources();
+        Drawable background = res.getDrawable(R.drawable.testing);
+
+//        View viewBackgroud = (View) findViewById(R.id.backgroud_view);
+//        viewBackgroud.setBackground(background);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.rellayout);
+        rl.setBackgroundDrawable(getResources().getDrawable(R.drawable.title_background));
     }
-
-//    @Nullable
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        final View view = inflater.inflate(R.layout.start_screen, container,false);
-//        ImageButton startbutton = (ImageButton) view.findViewById(R.id.start_button);
-//
-//        startbutton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                fragmentManager=getFragmentManager();
-//                view.setEnabled(false);
-//                transaction = fragmentManager.beginTransaction().add(new MainMenu).hide(
-//                        StartScreen.this
-//                ).addToBackStack(StartScreen.class.getName());
-//            }
-//        })
-//
-//        return view;
-//    }
-
 
     @Override
     public void onClick(View v) {
